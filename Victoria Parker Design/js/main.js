cf507438b39a0550e7a82c8297146fa82380b767
Victoria Parker <35713091+vicjpark@users.noreply.github.com>
2018-03-18 17:58:25 -0500

    Homework 7

--- Victoria Parker Design/js/main.js
+++ Victoria Parker Design/js/main.js
@@ -1,1 +1,28 @@
-// Make it rain!
+var imageNumber = 0;
+
+$('#next').on('click', function() {
+  if (imageNumber < 4) {
+    imageNumber += 1;
+  } else {
+    imageNumber = 0;
+  }
+
+  console.log(imageNumber);
+
+  $('img').hide();
+  $('img').eq(imageNumber).show();
+});
+
+
+$('#previous').on('click', function() {
+  if (imageNumber > 0) {
+    imageNumber -= 1;
+  } else {
+    imageNumber = 4;
+  }
+
+  console.log(imageNumber);
+
+  $('img').hide();
+  $('img').eq(imageNumber).show();
+});
